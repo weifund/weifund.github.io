d348a72b619dc2a0800cf8a200262b8075340510
SilentCicero <silentcicero@outlook.com>
2015-02-05 11:21:15 -0500

    Update CrowdFundrr.js

--- js/CrowdFundrr.js
+++ js/CrowdFundrr.js
@@ -62,6 +62,6 @@ function new_campaign()
 function get_campaign()
 {
   var c_id = $('#campaign_id').val();
-  var getCampaign = contract.call().getCampain(c_id);
+  var getCampaign = contract.call().getCampain(2);
   alert(getCampaign);
 }
