a0683631506b8063b70dc935341736eca7c8354f
SilentCicero <silentcicero@outlook.com>
2015-02-05 22:18:32 -0500

    Update CrowdFundrr.js

--- js/CrowdFundrr.js
+++ js/CrowdFundrr.js
@@ -62,10 +62,11 @@ function new_campaign()
   
   if(String(c_beneficiary).length == 0)
   {
-  	c_beneficiary = eth.accounts[0];
+  	var accounts = web3.eth.accounts;
+  	c_beneficiary = accounts[0];
   }
   
-  var new_camp = contract.transact().newCampaign(c_name, c_website, String(c_beneficiary), parseInt(c_goal), 776234786234); //parseInt(c_timelimit)
+  var new_camp = contract.transact().newCampaign(c_name, c_website, String(c_beneficiary), 50000, 124237892); //parseInt(c_timelimit)
   alert(new_camp);
 }
 
